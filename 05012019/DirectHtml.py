2fd168cb22053d6035bacc7fd197d405de41a984
Xaos <32937400+XaosFortuna@users.noreply.github.com>
2019-01-08 17:21:52 +0500

    Add files via upload

--- 05012019/DirectHtml.py
+++ 05012019/DirectHtml.py
@@ -4,11 +4,6 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import plotly as py
 
-# <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-# <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-# <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-# <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
 
 
 # external JavaScript files
@@ -31,6 +26,11 @@ external_scripts = [
 },
 {
 'src' : 'https://use.fontawesome.com/releases/v5.3.1/js/all.js'
+},
+{
+'src' : 'https://code.jquery.com/jquery-3.3.1.js',
+'integrity' : 'sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=',
+'crossorigin' : 'anonymous'
 }]
 
 # external CSS stylesheets
@@ -38,7 +38,7 @@ external_stylesheets = [
 {
 'rel' : 'stylesheet', 
 'href' : 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css',
-'integrity' : 'ha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS',
+'integrity' : 'sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS',
 'crossorigin' : 'anonymous'
 }, 
 {
@@ -47,14 +47,10 @@ external_stylesheets = [
 
 }]
 
-# add meta tags to header
 
+app = dash.Dash(__name__, external_scripts=external_scripts, external_stylesheets=external_stylesheets) 
 
-app = dash.Dash(__name__,
-	external_scripts=external_scripts,
-	external_stylesheets=external_stylesheets) 
-
-
+# add tags to header
 app.head = [
 html.Meta(
 	name = 'viewport', 
@@ -62,24 +58,30 @@ html.Meta(
 	),
 html.Meta(
 	charSet = 'utf-8'
-	)
-]
+	)]
 
 # add tags to footer
 app.footer = []
 
+
+
 app.layout = html.Section([
+	html.Div([
+		html.Div([
+			html.H1(['Hello world !'], className='col-12 d-flex justify-content-center text-warning bg-dark font-weight-lighter m-5 align-content-center flex-wrap'), 
+			html.Div(['btn'], className = 'btn btn-secondary m-5'),
+			html.Div(['bulma Primary'], className = 'button is-danger m-5')
+			], className='row'), 
+		html.Div([
 			html.Div([
-				html.Div([
-					html.H1(['Hello world !'], className=['col-12 d-flex justify-content-center text-warning bg-dark font-weight-lighter m-5 align-content-center flex-wrap']), 
-					html.Div(['btn'], className = ['btn btn-secondary m-5']),
-					html.Div(['bulma Primary'], className = ['button is-danger m-5'])
-					], 
-					className=['row'])
-				], 
-				className = ['container']
-				)
-			], className = ['hero is-dark is-fullheight']#, style = {'background-color' : '#250000'}
+				html.H1(['second row'.title()]),
+				html.Canvas(['Canvas Children'], id = 'BarChart', width = '400', height = '400'),
+				html.Script(['jQuery(document).ready(function($) {BarChart();});'], src = 'BarChart.js', type='text/javascript')
+				], className = 'col-4')
+			], className = 'row')
+		], className = 'container'
+		)
+			], className = 'hero is-dark is-fullheight'#, style = {'background-color' : '#250000'}
 			)
 
 
